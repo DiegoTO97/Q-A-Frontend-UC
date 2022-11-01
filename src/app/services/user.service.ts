a8d5586c551efe78b3e4bf1311ac5a841c55cb30
DiegoTO97 <ditoalpacaman@gmail.com>
2022-11-01 16:51:59 -0400

    Create endPoint variable

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  myAppUrl: string;
+  myApiUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.myAppUrl = environment.endPoint;
+    this.myApiUrl = '/api/user';
+  }
+
+  saveUser(user: User): void
+  {
+
+  }
 }
