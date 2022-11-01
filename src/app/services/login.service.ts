e71d93ce441037dfbe9bca0dd045e03cfa3f37cf
DiegoTO97 <ditoalpacaman@gmail.com>
2022-11-01 19:14:02 -0400

    Create and implement login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  myAppUrl: string;
+  myApiUrl: string;
 
-  constructor() { }
+
+  constructor(private http: HttpClient) 
+  { 
+    this.myAppUrl = environment.endPoint;
+    this.myApiUrl = '/api/Login';
+  }
+
+  login(user: User): Observable<any>
+  {
+    return this.http.post(this.myAppUrl + this.myApiUrl, user);
+  }
 }
