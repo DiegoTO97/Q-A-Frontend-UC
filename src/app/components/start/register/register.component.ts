900e7c6475a31504faf12f1b386ceabeb6c6e4e0
DiegoTO97 <ditoalpacaman@gmail.com>
2022-11-01 17:13:27 -0400

    Add userService

--- src/app/components/start/register/register.component.ts
+++ src/app/components/start/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,8 @@ export class RegisterComponent implements OnInit {
 
   register: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder,
+              private userService: UserService) { 
     this.register = this.fb.group({
       user: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -23,6 +26,15 @@ export class RegisterComponent implements OnInit {
 
   registerUser(): void {
     console.log(this.register);
+
+    const user: User = {
+      userName: this.register.value.user,
+      password: this.register.value.password
+    }
+
+    this.userService.saveUser(user).subscribe(data => {
+      console.log(data);
+    });
   }
 
   checkPassword(group: FormGroup): any{
