2e5f8eeb21683b7386e00bbfae6abbbacb5d3c16
DiegoTO97 <ditoalpacaman@gmail.com>
2022-11-01 17:13:57 -0400

    Make private my Http var

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,14 +8,15 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class UserService {
-  myAppUrl: string;
-  myApiUrl: string;
+  private myAppUrl: string;
+  private myApiUrl: string;
 
   constructor(private http: HttpClient) { 
     this.myAppUrl = environment.endPoint;
     this.myApiUrl = '/api/user';
   }
 
+  //http://localhost:5205/api/user
   saveUser(user: User): Observable<any>
   {
     return this.http.post(this.myAppUrl + this.myApiUrl, user);
